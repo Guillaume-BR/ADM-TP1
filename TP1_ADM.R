8e48a7173b461e1ff869c281d1fb484219a04a0e
Guillaume-BR <guillaume.bernardreymond@gmail.com>
2023-10-12 17:11:22 +0200

    fichier tex

--- TP1_ADM.R
+++ TP1_ADM.R
@@ -1,7 +1,11 @@
-wine=read.csv('wine.csv')
+library(xtable)
+wine=read.csv('~/ADM/ADM-TP1/wine.csv')
+xtable(wine[1:4,1:5], type = "latex", file = "wine.tex",digits = 3,
+        caption = "Extrait du tableau")
+
 #Mean and standard-deviation of the 29 quantitative variable in wine;
 M = unname(colMeans(wine[4:32]))
-V = unname(sapply(wine[4:32],sd)) #variance corrigée ?? facteur (21/20) ?
+V = unname(sapply(wine[4:32],sd))*21/20 #variance corrigée ?? facteur (21/20) ?
 print(V)
 print(M)
 
@@ -12,17 +16,25 @@ for (i in 1:29)
   CR[,3+i] <- (wine[,3+i]-M[i])/(V[i])
 }
 #Check if variables ar indeed recuded and centered
-Barycentre=(colMeans(CR[4:32]))
-print(Barycentre)
-Variance=diag(var(CR[4:32]))
+Barycentre=colMeans(CR[4:32])
+xtable(t(as.matrix(Barycentre[,1:5])), type = "latex", file = "barycentre.tex",digits = 3,
+       caption="extrait du tableau des barycentres")
+Variance=t(diag(var(CR[4:32])))
 print(Variance)
+xtable(t(as.matrix(Variance[,1:5])), type = "latex", file = "barycentre.tex",digits = 3,
+       caption="extrait du tableau des variances")
 
 #inertia
 Inertie=sum(Variance)
 print(Inertie)
 
+
+
 "Q2"
 chinon=CR[CR$Label == 'Chinon',]
+xtable(chinon[1:3,1:5], type = "latex", file = "barycentre.tex",digits = 3,
+       caption="extrait du tableau de l'appelation Chinon")
+
 saumur=CR[CR$Label == 'Saumur',]
 bourgueuil=CR[CR$Label == 'Bourgueuil',]
 
@@ -30,14 +42,18 @@ bourgueuil=CR[CR$Label == 'Bourgueuil',]
 pchi=nrow(chinon)/nrow(CR)
 psau=nrow(saumur)/nrow(CR)
 pbou=nrow(bourgueuil)/nrow(CR)
-#barycentre
+
+#barycentre des classes 
 mchi= unname(colMeans(chinon[4:32]))
+print(mchi)
 msau= unname(colMeans(saumur[4:32]))
 mbou= unname(colMeans(bourgueuil[4:32]))
+
 #carrée des normes euclidiennes des barycentres
 nchi=sum(mchi^2)
 nsau=sum(msau^2)
 nbou=sum(mbou^2)
+
 #Inertie externe
 Inex=pchi*nchi+psau*nsau+pbou*nbou
 #R2 pour le partionnement en appelation A CORRIGER
@@ -45,12 +61,23 @@ R=Inex/Inertie
 print(100*R)
 
 #R2 par variable
-rchi=(sapply((chinon[4:32]),sd)^2)/sum(Variance)
+RVariables=pchi*mchi^2+psau*msau^2+pbou*mbou^2
+print(RVariables)
+
+pchi*mchi[1]^2 + pbou*mbou[1]^2 + psau*msau[1]^2
+sum(1/29*RVariables)
+
+rchi=(sapply((chinon[4:32]),sd)^2)/sum(Variance) #pas bon ici
+rchibis=chinon[4:32]
 rbou=(sapply((bourgueuil[4:32]),sd)^2)/sum(Variance)
 rsau=(sapply((saumur[4:32]),sd)^2)/sum(Variance)
 trichi=rchi[order(unlist(rchi))]
 tribou=rbou[order(unlist(rbou))]
 trisau=rsau[order(unlist(rsau))]
+
+print(as.matrix(tribou))
+print(as.matrix(trichi))
+print(as.matrix(trisau))
 
 plot(trichi,xaxt='n')
 axis(1,at=1:29,labels=names(rchi))
@@ -63,7 +90,6 @@ axis(1,at=1:29,labels=names(rsau))
 
 Rr=1-(sum(rchi))
 print(Rr)
-rm(red)
 
 #PART2
 
