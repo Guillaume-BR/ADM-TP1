bc47e26b4e696beddb855f8e69c1205bf9355389
Guillaume-BR <guillaume.bernardreymond@gmail.com>
2023-10-18 15:34:59 +0200

    the end ?

--- TP1_ADM.R
+++ TP1_ADM.R
@@ -3,34 +3,32 @@ wine=read.csv('~/ADM/ADM-TP1/wine.csv')
 xtable(wine[1:4,1:5], type = "latex", file = "wine.tex",digits = 3,
        caption = "Extrait du tableau")
 
-
-#Moyenne M et deviation-standard V des 29 variables quantitatives de Wine
+#Mean and standard-deviation of the 29 quantitative variable in wine;
 M = unname(colMeans(wine[4:32]))
-V = unname(sapply(wine[4:32],sd))*sqrt(20/21) #correction du facteur sqrt(1/(n-1)) utilisé par l'estimateur sd()
+V = unname(sapply(wine[4:32],sd))*sqrt(20/21) #variance corrigée ?? facteur (21/20) ?
+print(V)
+print(M)
 
-#Tableau CR des 29 variables quantitatives de Wine centrée-réduite 
+#Centering and reducing
 CR=wine
 for (i in 1:29)
 {
   CR[,3+i] <- (wine[,3+i]-M[i])/(V[i])
 }
-
-#Confirmation que CR soit centré-réduit
+#Check if variables ar indeed recuded and centered
 Barycentre=colMeans(CR[4:32])
-Variance=diag(var(CR[4:32])*20/21) #correction du facteur 1/(n-1) utilisé de l'estimateur var()
+Variance=diag(var(CR[4:32])*20/21)
 print(Variance)
-print(Barycentre)
-
-#Inertie
+#inertia
 Inertie=sum(Variance)
 print(Inertie)
 
-#Partition de CR en 3 tableaux d'appellation
+"Q2"
 chinon=CR[CR$Label == 'Chinon',]
 saumur=CR[CR$Label == 'Saumur',]
 bourgueuil=CR[CR$Label == 'Bourgueuil',]
 
-#poids de chaques classes d'apellation
+#poids
 pchi=nrow(chinon)/nrow(CR)
 psau=nrow(saumur)/nrow(CR)
 pbou=nrow(bourgueuil)/nrow(CR)
@@ -45,22 +43,20 @@ nchi=sum(mchi^2)
 nsau=sum(msau^2)
 nbou=sum(mbou^2)
 
-#Inertie inter-appelation
+#Inertie externe
 Inex=pchi*nchi+psau*nsau+pbou*nbou
-
-#R2 de le partitionement en appellation
+Inex
+#R2 pour le partitionement en appellation
 R=Inex/Inertie
 print(100*R)
 
-#Tableau rvar du R2 de chaques variables sensorielles
-rvar=pchi*mchi^2+psau*msau^2+pbou*mbou^2
-
-#Graphique des variables en fonction de leur R2, classées par ordre croissant
+#R2 par variable
+rvar=pchi*mchi^2+psau*msau^2+pbou*mbou^2  #via normes euclidiennes
+print(rvar)
 trirvar=rvar[order(unlist(rvar))]
-par(mar=c(10,5,2,5))
-plot(trirvar,xaxt='n',xlab='',ylab ='R²')
+trirvar
+plot(trirvar,xaxt='n')
 axis(1,at=1:29,labels=FALSE,srt=90)
-<<<<<<< HEAD
 text(0.5:28.5,rep(-0.05,2),labels=names(trirvar),srt = 45,xpd=NA,adj=c(1,1))
 sum(rvar*1/29)# c'est bon ici !!
 
@@ -68,59 +64,41 @@ sum(rvar*1/29)# c'est bon ici !!
 
 #Q1
 #def (a faire label)
-=======
-text(0.5:28.5,rep(-0.05,2),labels=names(trirvar),adj = c(0.6,3.2),srt = 90,xpd=NA,cex=0.75)
-
-#Moyenne arithmétiques des R2
-moy_ravr=sum(rvar*1/29)
-
-#Creation des matrices W,M,X,Y,Z
->>>>>>> 1130b40bbd47960334a5ee42deebe17689f6499b
 w=1/nrow(wine) * diag(1,nrow(wine))
 m=1/ncol(wine[4:32]) * diag(1,ncol(wine[4:32]))
 X=as.matrix(CR[4:32])
 Y=cbind(ifelse(CR$Label == 'Bourgueuil',1,0),ifelse(CR$Label == 'Chinon',1,0),ifelse(CR$Label == 'Saumur',1,0))
 Z=cbind(ifelse(CR$Soil == 'Env1',1,0),ifelse(CR$Soil == 'Env2',1,0),ifelse(CR$Soil == 'Reference',1,0),ifelse(CR$Soil == 'Env4',1,0))
 
-#Matrice Py du projecteur de Y
+#piYw
 Py= Y %*% solve(t(Y) %*% w %*% Y) %*% t(Y) %*% w
 print(Py)
 
-#Liste Px des projecteurs Px[j] de chaques Xj
+#piXjw
+rm(j)
 Px=list()
 for (j in 1:29) {
   Px[[j]] = X[,j] %*% solve(t(X[,j]) %*% w %*% X[,j]) %*% t(X[,j]) %*% w
 }
-#Tableau Ty des Tr(Px[j] * Py)
-Ty=c()
+#tr
 for (i in 1:29)
 {
-  Ty[i]=sum(diag(Px[[i]]%*%Py))
+  T[i]=sum(diag(Px[[i]]%*%Py))
 }
-Ty=as.matrix(t(Ty))
-colnames(Ty) <- colnames(CR[4:32])
 
-#Comparaison avec rvar
-print((unname(Ty)))
+print(T)
+
+xtable(as.matrix(t(T[1:4]), type = "latex", file = "wine.tex",digits = 3,
+       caption = "Extrait du tableau")
+
 print(unname(rvar))
-<<<<<<< HEAD
 #C'est bon, il coïncide avec rvar de la première partie !
-=======
->>>>>>> 1130b40bbd47960334a5ee42deebe17689f6499b
 
-#Calcule de Tr(XMX'W*Py)
+#QC
 Rma=X %*% m %*% t(X) %*% w
-Rmat=sum(diag(Rma %*% Py))
+sum(diag(Rma %*% Py))
 
-#Creation de la matrice Pz du projecteur de Z
+#Q2
 Pz= Z %*% solve(t(Z) %*% w %*% Z) %*% t(Z) %*% w
+sum(diag(Rma %*% Pz))
 
-#Calcule de Tr(XMX'W*Pz)
-Rs=sum(diag(Rma %*% Pz))
-
-#Tableau Tz des Tr(Px[j] * Pz)
-Tz=c()
-for (i in 1:29)
-{
-  Tz[i]=sum(diag(Px[[i]]%*%Pz))
-}
