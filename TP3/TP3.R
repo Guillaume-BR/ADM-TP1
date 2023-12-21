9c057242c3ac561676c872606d4882f0fbc79546
Guillaume-BR <guillaume.bernardreymond@gmail.com>
2023-12-21 11:42:24 +0100

    risque et nature

--- TP3/TP3.R
+++ TP3/TP3.R
@@ -2,10 +2,8 @@ library(Factoshiny)
 library(FactoMineR)
 #1)a) Chargement du dataframe
 villesbrut = read.table("~/ADM/ADM-TP1/TP3/villes.csv", header = TRUE, row.names=1)
-villes = Factoshiny(villesbrut)
 
 villes_rang = apply(villesbrut, MARGIN = 2 , FUN = rank)
-acp_rang = Factoshiny(villes_rang)
 
 villes_eco = villesbrut[,c('Chomage', "ChomageJeunes", "ChomageLong", "EvoluEmploiCree", "Activite", "EmploiFeminin",
                        "EmploiCommune", "DefaillEntreprise", "SalaireAnnuel", "ImpotRevenu", "ImpotFortune", "Imposables",
@@ -13,4 +11,12 @@ villes_eco = villesbrut[,c('Chomage', "ChomageJeunes", "ChomageLong", "EvoluEmpl
                        "AttiranceGlobale", "AttiranceActifs", "Proprietaires", "LogtSup4pieces", "LogtInsalubre", "LogtVacant",
                        "LogtConstruction")]
 
-acp_eco = 
+villes_ris = villesbrut[,c('Criminalite', 'EvolutionCrimes', 'SecuriteRoutiere', 'Inondations', 'TerrainsPollues', 'UsinesRisques',
+                           'MortaliteInfantile', 'MortaliteCancerPoumon', 'MortaliteAlcool', 'DecesInfarctus', 'TauxSuicide',
+                           'MortaliteGlobale', 'TailleClassesPrimaires', 'Retard6eme', 'Retard3eme', 'RetardTerminale')]
+
+villes_nat = villesbrut[ , c('Mer', 'Ski', 'Soleil', 'Pluie', 'Temperature', 'MarcheAPied')]
+
+villes_nat_rang = apply(villes_nat, MARGIN = 2 , FUN = rank)
+
+acp_nat_rang = Factoshiny(villes_nat_rang)
